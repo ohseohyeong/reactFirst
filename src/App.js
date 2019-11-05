0a9fc727a7d5386e5044b85686d4c16cfcf82712
ohseohyeong <yapssuo@gmail.com>
2019-11-05 21:21:51 +0900

    css

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return <div>Hello World</div>
-}
-asdfasd
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      <header>head</header>
+      <nav>nav</nav>
+      <div className="main">
+        <section>main</section>
+        <aside>aside</aside>
+      </div>
+      <footer>footer</footer>
+    </div>
+    
+    
+  );
+}
+
+export default App;
