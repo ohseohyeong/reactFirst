8384c770d089f217911afc4bef1242468d191c34
ohseohyeong <46581576+ohseohyeong@users.noreply.github.com>
2019-11-07 21:11:45 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,112 @@
 import React from 'react';
-import './App.css';
+import styled, {keyframes} from 'styled-components'
+// import styles from './App.module.scss';
 
-function App() {
+// const Root= styled.div`
+//   ${props=>!props.show && css`
+//   display:none;
+//   `
+// }
+// `;
+
+// const ShowDiv=styled.div`
+//   display: block;
+//   `;
+
+//   const NoShowDiv=styled.div`
+//   display:none;
+//   `;
+
+// const move=keyframes`
+// from{
+//   transform:translateX(0%);
+// }
+// to{
+//   transform:translateY(100%);
+// }
+// `;
+// const Img = styled.img`
+// animation: ${move} 1s linear infinite;
+// `;
+
+// function App(){
+//   // const show=true;
+//   return (
+//     <>
+//       <Img src={"https://image.flaticon.com/icons/svg/1383/1383676.svg"} />
+
+
+//     </>
+//   );
+// }
+
+//const fade=keyframes`
+//   from{
+//     opacity:1;
+//   }
+//   to{
+//     opacity:0;
+//   }
+// `;
+
+// const appear=keyframes`
+//   from{
+//     opacity:0;
+//   }
+//   to{
+//     opacity:1;
+//   }
+// `;
+
+// const Img = styled.img`
+// animation: ${fade?appear:fade} 1s linear infinite;
+// `;
+
+// function App(){
+//   const fade=true;
+//   return (
+//     <>
+//       <Img src={"https://image.flaticon.com/icons/svg/1383/1383676.svg"} />
+
+
+//     </>
+//   );
+// }
+
+const Wrapper=styled.div`
+  padding:15px;
+  display:flex;
+  align-item:center;
+  label{
+    font-size:20px;
+    margin-left:20px;
+  }
+
+  div{
+    display:none;
+  }
+`;
+
+
+const Input=styled.input`
+  &~div{
+    display: ${props=>(props.checked ? "block":"none")};
+  
+  }
+
+`;
+
+function App(){
+  // const fade=true;
+  const checked=false;
   return (
-    <div className="root">
-      <header>head</header>
-      <nav>nav</nav>
-      <div className="main">
-        <section>main</section>
-        <aside>aside</aside>
-      </div>
-      <footer>footer</footer>
-    </div>
-    
-    
+    <>
+      <Wrapper>
+        <Input type="checkbox" id="check1" checked={checked}/>
+        <label htmlFor="check1">pls check</label>
+      </Wrapper>
+
+    </>
   );
 }
 
